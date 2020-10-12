5273ed3ab36436a8eeabccee3ea70f440f95aca0
Hans Hoffman <hoffman.hans.j@gmail.com>
2020-10-12 14:45:23 -0400

    setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FC } from "react";
 
-function App() {
+import logo from "./logo.svg";
+import "./App.css";
+
+const App: FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img alt="logo" className="App-logo" src={logo} />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
           href="https://reactjs.org"
-          target="_blank"
           rel="noopener noreferrer"
-        >
+          target="_blank">
           Learn React
         </a>
       </header>
     </div>
   );
-}
+};
 
 export default App;
